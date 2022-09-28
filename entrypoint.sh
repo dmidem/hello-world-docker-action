36e013640d3011245a44cc89bcae3948c58cb409
Dmitry Demin <dmidem@users.noreply.github.com>
2022-09-28 14:03:10 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ echo "Hello!"
 
 ls -l /opt
 
-echo "Secret #1:"
+echo "Secret 1:"
 cat /opt/SERVICE_ACCOUNT_CREATE_PEM
 
 echo ""
 
-echo "Secret #2:"
+echo "Secret 2:"
 cat /opt/SERVICE_ACCOUNT_BACKUP_PEM
